d86cbc035fc1e21ae1f9dfcb5ad7f9ef598bfacb
jagod101 <jagod101@mail.chapman.edu>
2020-02-26 10:34:17 -0800

    Fixed Error in Description

--- Assignment2/TotalPrice.py
+++ Assignment2/TotalPrice.py
@@ -2,7 +2,7 @@
 #Student ID: 2274813 and 2314750
 #Student Email: jagod101@mail.chapman.edu and kavesh@chapman.edu
 #CPSC230
-#Assignment 1 - Part 1
+#Assignment 2 - Part 1
 
 ###The program should find the total price of an item given the price and sales tax.
 ###The program should prompt the user for the purchase price of an item and the sales tax rate,
@@ -20,7 +20,7 @@ purchasePrice = float(input("What was the Purchase Price of your Item: "))
 #Check to see if purchasePrice is greater than 0, continue if it is...
 if purchasePrice > 0:
     salesTax = float(input("What was the Sales Tax on your Item: "))
-    
+
     #Check to see if the salesTax is greater than 0, continue if it is...
     if salesTax > 0:
         #Calculate the Total Price
